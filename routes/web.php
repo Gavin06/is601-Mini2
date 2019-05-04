c3ea6a526d2ab5f1f0864970afcdf677102d65f6
gdp9 <gdp9@njit.edu>
2019-05-04 15:23:26 -0400

    Task4: Create a new model called car and add 3 properties to it: Make, Model, and year

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/test', 'HomeController@test')->name('test');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
